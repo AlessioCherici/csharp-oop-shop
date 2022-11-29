5c675d7a1e5b7246d4c3ae2ebd84b72e1b2c51d3
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-29 14:34:35 +0100

    Aggiungere i file di progetto.

---
+++ CSharpShop/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop
+    {
+    public class Prodotto
+        {
+        private string Nome;
+        private int NumeroProdotto;
+        private string Descrizione;
+        private double Prezzo;
+        private float Iva;
+        }
+    }
