87181c8f87fdd4aa3fb8d06a869d7361f10ccd10
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-29 17:17:07 +0100

    Creato elenco prodotti + codice prodotto padded left con leading 0 *fine*

---
+++ CSharpShop - Bonus/Class1.cs
@@ -0,0 +1,99 @@
+﻿
+namespace CSharpShop___Bonus
+    {
+    public class Prodotto
+        {
+        private string Nome;
+        private int NumeroProdotto;
+        private string Descrizione;
+        private double Prezzo;
+        private double Iva;
+
+        public Prodotto()
+            {
+            this.Nome = "Prodotto";
+            this.NumeroProdotto = 0;
+            this.Descrizione = string.Empty;
+            this.Prezzo = 0;
+            this.Iva = 0.0f;
+            }
+
+
+
+        //------------------------- Metodi Get -------------------------
+
+        public double GetPrezzo()
+            {
+            return this.Prezzo;
+            }
+
+        public string GetNome()
+            {
+            return this.Nome;
+            }
+
+        public int GetNumeroProdotto()
+            {
+            return NumeroProdotto;
+            }
+
+        public double GetIva()
+            {
+            return this.Iva;
+            }
+
+        //------------------------- Metodi Set -------------------------
+
+        public void SetNumeroProdotto()
+            {
+            this.NumeroProdotto = GeneratoreRGN();
+            }
+
+        public void SetPrezzo(double Prezzo)
+            {
+            this.Prezzo = Prezzo;
+            }
+
+        public void SetDescrizione(string Descrizione)
+            {
+            this.Descrizione = Descrizione;
+            }
+
+        public void SetNome(string Nome)
+            {
+            this.Nome = Nome;
+            }
+
+        public void SetIva(double Iva)
+            {
+            this.Iva = Iva;
+            }
+
+
+        //------------------------- Metodi Calcoli Vari -------------------------
+        public int GeneratoreRGN()
+            {
+            Random NumeroRNGProdotto = new Random();
+            int CodiceProdotto = NumeroRNGProdotto.Next(100000000);
+            return CodiceProdotto;
+            }
+        public double PrezzoIvato(double Prezzo, double Iva)
+            {
+            double ValoreIva = Prezzo * Iva / 100;
+            double PrezzoIvato = Prezzo + ValoreIva;
+            return Math.Round(PrezzoIvato, 2);
+            }
+
+        //------------------------- Metodo Stampa Prodotto -------------------------
+
+        public void StampaTutto()
+            {
+            Console.WriteLine("---------" + this.Nome + "---------" + "\n");
+            Console.WriteLine("Descrizione: " + this.Descrizione);
+            Console.WriteLine("Codice Prodotto: " + this.NumeroProdotto.ToString().PadLeft(12, '0'));      //Stampa di codice prodotto in padding left con leading 0's     *BONUS*
+            Console.WriteLine("Prezzo prodotto: " + this.Prezzo + " Euro");
+            Console.WriteLine("Iva prodotto: " + this.Iva + "%");
+            Console.WriteLine("Prezzo con Iva: " + PrezzoIvato(this.Prezzo, this.Iva) + "\n");
+            }
+        }
+    }
---
+++ CSharpShop - Bonus/Program.cs
@@ -0,0 +1,97 @@
+﻿
+/*Nel progetto CSharpShop , creare la classe Prodotto che gestisce i prodotti del vostro shop.
+Un prodotto è caratterizzato da:
+codice(numero intero)
+nome
+descrizione
+prezzo
+iva
+
+
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva.
+
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interagirci con loro per testare tutti i metodi che avete previsto e implementato.*/
+
+using CSharpShop___Bonus;
+
+/*Prodotto Prodotto1 = new Prodotto();
+
+Console.WriteLine("Benvenuto nel sistema di generazione prodotti");
+
+Console.WriteLine("Inserisci il nome del prodotto che vuoi registrare");
+string NomeInserito = Console.ReadLine();
+Prodotto1.SetNome(NomeInserito);
+
+Prodotto1.SetNumeroProdotto();
+
+
+Console.WriteLine("Ora inserisci la descrizione del prodotto");
+string DescrizioneInserita = (Console.ReadLine());
+Prodotto1.SetDescrizione(DescrizioneInserita);
+
+Console.WriteLine("Ora inserisci il prezzo del prodotto");
+double PrezzoInSerito = double.Parse(Console.ReadLine());
+Prodotto1.SetPrezzo(PrezzoInSerito);
+
+Console.WriteLine("Ora inserisci l'Iva del prodotto (senza il simbolo %)");
+double IvaInserita = double.Parse(Console.ReadLine());
+Prodotto1.SetIva(IvaInserita);
+
+Prodotto1.StampaTutto();*/
+
+List<Prodotto> list = new List<Prodotto>();
+
+Prodotto Prodotto2 = new Prodotto();
+Prodotto2.SetNome("Outer Wilds");                     //Dovresti davvero giocarlo Emanuele, e' un capolavoro!
+Prodotto2.SetNumeroProdotto();
+Prodotto2.SetDescrizione("Outer Wilds e' un videogioco di esplorazione spaziale," + "\n" + 
+    "incentrato sullo svolgimento di una trama misteriosa ed intricata!");
+Prodotto2.SetPrezzo(22.90d);                          //Veramente poco,considerato il capolavoro che giocherai!
+Prodotto2.SetIva(22);
+Prodotto2.StampaTutto();
+
+Prodotto Prodotto3 = new Prodotto();
+Prodotto3.SetNome("Doom Eternal");                     
+Prodotto3.SetNumeroProdotto();
+Prodotto3.SetDescrizione("Doom Eternal è uno sparatutto in prima persona in cui interpreti" + "\n" +
+    "il ruolo del DemonSlayer, atto a salvare il mondo da un invasione di demoni!");
+Prodotto3.SetPrezzo(56d);                          
+Prodotto3.SetIva(22);
+Prodotto3.StampaTutto();
+
+Prodotto Prodotto4 = new Prodotto();
+Prodotto4.SetNome("Escape From Tarkov");
+Prodotto4.SetNumeroProdotto();
+Prodotto4.SetDescrizione("Escape From Tarkov è l' esperienza simulativa FPS definitiva, ambientato in una" + "\n" +
+    "zona ristretta in Russia, dove non ci sono regole e lo scopo principale e' sopravvivere!");
+Prodotto4.SetPrezzo(45.50d);
+Prodotto4.SetIva(22);
+Prodotto4.StampaTutto();
+
+Prodotto Prodotto5 = new Prodotto();
+Prodotto5.SetNome("CyberPunk 2077");
+Prodotto5.SetNumeroProdotto();
+Prodotto5.SetDescrizione("CyberPunk 2077 e' ambientato in un lontano futuro distopico disfattista dove le persone" + "\n" +
+    "indossano augment cibernetici per incrementare forza, intelligenza, velocità, spesso a discapito della loro salute /sanità mentale ");
+Prodotto5.SetPrezzo(35.60d);
+Prodotto5.SetIva(22);
+Prodotto5.StampaTutto();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
