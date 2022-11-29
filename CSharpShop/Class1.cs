211c60cbc0d654ae73ff8fcf4307d860ee8b1bc5
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-29 15:46:21 +0100

    Inseriti primi dati in set, definiti primi moduli

--- CSharpShop/Class1.cs
+++ CSharpShop/Class1.cs
@@ -13,5 +13,66 @@ namespace CSharpShop
         private string Descrizione;
         private double Prezzo;
         private float Iva;
+
+        public Prodotto()
+            {
+            this.Nome = "Prodotto";
+            this.NumeroProdotto = 0;
+            this.Descrizione = string.Empty;
+            this.Prezzo = 0;
+            this.Iva = 0.0f;
+            }
+
+
+
+        //------------------------- Metodi -------------------------
+
+        public int GeneratoreRGN()
+            {
+            Random NumeroRNGProdotto = new Random();
+            int CodiceProdotto = NumeroRNGProdotto.Next(100000000);
+            return CodiceProdotto;
+            }
+
+        public double GetPrezzo()
+            {
+            return this.Prezzo;
+            }
+
+        public string GetNome()
+            {
+            return this.Nome;
+            }
+
+        public int GetNumeroProdotto()
+            {
+            return NumeroProdotto;
+            }
+
+        public float GetIva()
+            {
+            return this.Iva;
+            }
+
+        public void SetPrezzo(double Prezzo)
+            {
+            this.Prezzo = Prezzo;
+            }
+
+        public void SetDescrizione(string descrizione )
+            {
+            this.Descrizione = Descrizione;
+            }
+
+        public void SetNome(string Nome)
+            {
+            this.Nome = Nome;
+            }
+
+        public void SetIva(float Iva)
+            {
+            this.Iva = Iva;
+            }
+
         }
     }
